a8148612641cd7a27ae1adb91f8d46ffc5e525cf
Lucas Borio <lucasborio@gmail.com>
2022-08-23 20:56:35 -0300

    initial

---
+++ src/lib/Search.svelte
@@ -0,0 +1,40 @@
+<script>
+  let searchstring = '';
+  let engine = 'ecosia';
+  const searchengines = [
+    { title: 'Ecosia', key: 'ecosia', url: 'https://ecosia.org/search?q=' },
+    { title: 'Google', key: 'google', url: 'https://google.com/search?q=' },
+    { title: 'DuckDuckGo', key: 'ddgo', url: 'https://duckduckgo.com/?q=' }
+  ];
+  function search() {
+    const eng = searchengines.find((val) => val.key === engine);
+    if (searchstring) window.location.assign(eng.url + searchstring);
+  }
+</script>
+
+<div class="flex-container">
+  <input
+    bind:value={searchstring}
+    type="text"
+    placeholder="search"
+    id="name_field"
+    class="nes-input"
+  />
+  <button type="button" class=" btn nes-btn is-primary" on:click={search}>Search</button>
+</div>
+<div>
+  <!-- or with adnditional index value -->
+  {#each searchengines as item, i}
+    <label>
+      <input type="radio" class="nes-radio" bind:group={engine} value={item.key} />
+      <span>{item.title}</span>
+    </label>
+  {/each}
+</div>
+
+<style>
+  .btn {
+    box-sizing: border-box;
+    display: inline-block;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { onMount } from 'svelte';
+  import Search from '$lib/Search.svelte';
+  // Inject the time in the UI
+  const renderTime = function () {
+    let time = new Date();
+    return time.toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true
+    });
+  };
+
+  let clock = renderTime();
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      clock = renderTime();
+    }, 1000);
+  });
+</script>
+
+<nav style="padding: 20px; border-bottom: 3px solid #ccc; margin-bottom: 60px;">
+  <div class="container">
+    <h2 style="margin-bottom: 0;">
+      <i class="snes-jp-logo" />
+      8Bit-Home
+    </h2>
+  </div>
+</nav>
+
+<main class="flex-container2">
+  <div id="clock" class="flex-item1">{clock}</div>
+  <Search />
+</main>
